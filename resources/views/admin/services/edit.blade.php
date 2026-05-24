0cbe8922040047b95a51cf3b0c8b2a096f82834b
AnnaMmcc <urosevicanja913@gmail.com>
2026-05-24 14:34:12 +0200

    *Added style to edit page

--- resources/views/admin/services/edit.blade.php
+++ resources/views/admin/services/edit.blade.php
@@ -1,40 +1,116 @@
-@include('admin.partials.admin-navbar')
+@extends('layouts.admin')
 
-<h1>Edit Service</h1>
+@section('content')
 
-@if ($errors->any())
-    <div style="color:red;">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+<div class="max-w-3xl">
 
-<form action="{{ route('admin.services.update', $service->id) }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
+    <h1 class="text-3xl font-bold mb-8">
+        Edit Service
+    </h1>
 
-    <label>Title:</label>
-    <input type="text" name="title" value="{{ old('title', $service->title) }}" required><br><br>
+    @if ($errors->any())
 
-    <label>Description:</label>
-    <textarea name="description" required>{{ old('description', $service->description) }}</textarea><br><br>
+        <div class="bg-red-100 border border-red-300 text-red-700 p-4 rounded-xl mb-6">
 
-    <label>Price:</label>
-    <input type="number" step="0.01" name="price" value="{{ old('price', $service->price) }}"><br><br>
+            <ul class="list-disc list-inside">
 
-    <label>Image:</label>
-    <input type="file" name="image"><br>
-    @if($service->image)
-        <img src="{{ asset('storage/'.$service->image) }}" width="100"><br>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+
+            </ul>
+
+        </div>
+
     @endif
 
-    <button type="submit">Update</button>
-</form>
+    <form
+        action="{{ route('admin.services.update', $service) }}"
+        method="POST"
+        enctype="multipart/form-data"
+        class="bg-white rounded-2xl shadow p-8 space-y-6"
+    >
 
-<a href="{{ route('admin.services.index') }}">Back to list</a>
 
+     @csrf
+     @method('PUT')
+        <div>
 
+            <label class="block mb-2 font-semibold">
+                Title
+            </label>
 
+            <input
+                type="text"
+                name="title"
+                value="{{ old('title', $service->title) }}"
+                class="w-full border border-gray-300 rounded-xl p-3"
+                required
+            >
+
+        </div>
+
+        <div>
+
+            <label class="block mb-2 font-semibold">
+                Description
+            </label>
+
+            <textarea
+                name="description"
+                rows="5"
+                class="w-full border border-gray-300 rounded-xl p-3"
+                required
+            >{{ old('description', $service->description) }}</textarea>
+
+        </div>
+
+        <div>
+
+            <label class="block mb-2 font-semibold">
+                Price
+            </label>
+
+            <input
+                type="number"
+                step="0.01"
+                name="price"
+                value="{{ old('price', $service->price) }}"
+                class="w-full border border-gray-300 rounded-xl p-3"
+            >
+
+        </div>
+
+        <div>
+
+            <label class="block mb-2 font-semibold">
+                Image
+            </label>
+
+            <input
+                type="file"
+                name="image"
+                class="w-full"
+            >
+         @if($service->image)
+
+         <img
+          src="{{ asset('storage/'.$service->image) }}"
+          class="w-40 rounded-xl mt-4"
+         >
+
+         @endif
+        </div>
+
+        <button
+            type="submit"
+            class="bg-black text-white px-6 py-3 rounded-xl"
+        >
+            Update Service
+        </button>
+
+    </form>
+
+</div>
+
+@endsection
